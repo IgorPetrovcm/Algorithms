d5ea3b908236be8fd03fa48c956e023d86160812
igoradm <igorpetrov42ru@gmail.com>
2023-10-28 23:34:54 +0300

    new binary search and test opportunities

---
+++ BinarySearch2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BynarySearch 
+{
+    using System;
+    class Program 
+    {
+         static void Main(string[] args) 
+         {
+            Console.Write("How many users to add: ");
+            byte usersQuantity = byte.Parse(Console.ReadLine());
+
+            Users[] usersArr = new Users[usersQuantity];
+            
+         }
+    }
+
+    class Users 
+    {
+        public string Name {get; set;}
+        public byte Id {get; set;}
+    }
+}
---
+++ TestingOpportunities/RandomCharAlphabet/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RandomChar 
+{
+    using System ;
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Random random = new Random();
+            char a = Convert.ToChar(random.Next('a','z'+1));            
+            char b = (char)random.Next('a','z'+1);
+            byte c = (byte)random.Next('a','z'+1);
+            char d = (char)c;
+            
+            Console.WriteLine($"{a}\n{b}\n{c}\n{d}");
+        }
+    }
+}
