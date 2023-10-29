017c55f66847fed5c89ffcd122959b5c165090dd
igoradm <igorpetrov42ru@gmail.com>
2023-10-30 00:58:44 +0300

    reverse register

---
+++ EdabitStudy/ReverseRegisterText/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Program
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "Hello World!";
+            string txtReverse = null;
+
+            for (int i = 0; i < txt.Length;i++) 
+            {
+                char a = char.ToLower(txt[i]);
+                if (a == txt[i]) {
+                    txtReverse += char.ToUpper(a);
+                }
+                else {
+                    txtReverse += a;
+                }
+            } 
+
+            Console.Write(txtReverse);
+        }
+    }
+}
