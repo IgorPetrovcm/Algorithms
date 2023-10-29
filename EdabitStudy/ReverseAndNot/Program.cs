3feede6608f07a73d6279aa00f9b4065e5ba88c3
igoradm <igorpetrov42ru@gmail.com>
2023-10-30 01:34:35 +0300

    first way 'ReverseAndNot'

---
+++ EdabitStudy/ReverseAndNot/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Program
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //два способа решения?
+            //Первый спобособ
+            int i = 123;
+            string iStr = i.ToString();
+            string result = iStr;
+
+            for (int j = iStr.Length-1; j >= 0; j--) 
+            {
+                result += iStr[j];
+            }
+            result += iStr;
+
+            Console.WriteLine(result);
+        }
+    }
+}
