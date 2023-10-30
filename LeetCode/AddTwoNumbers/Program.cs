963b6d77c1ad66e344bfa4e6ed7bdfd47e3d0627
igoradm <igorpetrov42ru@gmail.com>
2023-10-31 01:44:22 +0300

    start job

---
+++ LeetCode/AddTwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿ namespace Program
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode l01;
+            
+            ListNode l1 = new ListNode(1,);
+        }
+    }
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) {
+            this.val = val;
+            this.next = next;
+        }
+}
