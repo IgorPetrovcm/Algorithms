debffd4c0768a53741cc9e1f086a1401ffc28a5a
igoradm <igorpetrov42ru@gmail.com>
2023-11-01 19:59:32 +0300

    Начал работу над алгоритмом поиска цифры в числах, хочу занять как можно меньше памяти и поднять производительно, поэтому буду работать с числами на прямую

---
+++ EdabitStudy/SevenBoom!/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Program
+{
+    using System;
+    using System.Security.AccessControl;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arr = new int[rand.Next(0,20)];
+
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                arr[i] = rand.Next(0,100000);
+                Console.Write($"{arr[i]}  ");
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int remainder;
+                while ()
+
+            }
+        }
+    }
+}
