4da37cf43db76bbb57cef8fa00dd95f5bcdddbc2
igoradm <igorpetrov42ru@gmail.com>
2023-11-01 22:37:57 +0300

    Algorithm Palidron number__slow

---
+++ LeetCode/PalindromeNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿ namespace Program
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 121;
+            string a = val.ToString();
+            string aReverse = "";
+            for (int i = a.Length-1; i > -1; i--) aReverse += a[i];   
+                     
+            if (aReverse == a) System.Console.WriteLine("true");     
+            else System.Console.WriteLine("false");
+        }
+    }
+}
