b79e004093d877ee4fc60e7eb0b89ed7ae94b8b1
igoradm <igorpetrov42ru@gmail.com>
2023-11-02 00:14:37 +0300

    Нашел оптимальное решение, узнал про очень быстрый метод Sort класса Array

--- LeetCode/CanMakearIthmatic/Program.cs
+++ LeetCode/CanMakearIthmatic/Program.cs
@@ -1,15 +1,18 @@
 ﻿ namespace Program
 {
     using System;
+    using System.Formats.Asn1;
+
     class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = {2,4,6,3};
+            int[] arr = {3,5,1};
             int[] arr2 = {1,4,7};
             int[] arr3 = {6,3,0};
 
-            //O(n)
+            //O(n) изначально было без Array.Sort, поэтому работало не правильно, массив не был отсортирован. Для Sort код сложный и плохой
+            /*Array.Sort(arr);
             int abs = 0;
             if (1 < arr.Length) 
             {
@@ -29,7 +32,21 @@
                 else break;
 
             }
-            Console.WriteLine("true");
+            Console.WriteLine("true"); */
+
+            //O(n)?
+            Array.Sort(arr);
+
+            int abs = 0;
+            if (1 < arr.Length) {
+                abs = arr[0] - arr[1];
+            }
+            //else return true;
+            for (int i = 1; i < arr.Length - 1; i++) 
+            {
+                if (arr[i] - arr[i+1] != abs) Console.Write("false");
+            }
+            Console.Write("true");
         }
     }
 }
