6991f65bc70ac095c6e16dee6324620c9eab080f
igoradm <igorpetrov42ru@gmail.com>
2023-11-02 22:42:31 +0300

    Two product problem

---
+++ EdabitStudy/TwoProductProblem/Program.cs
@@ -0,0 +1,27 @@
+﻿ namespace Program
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1,2,3,4,13,5};
+            int[] arr2 = new int[2];
+            int n = 39;
+
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                for (int j = i; j < arr.Length; j++) 
+                {
+                    if (arr[i] + arr[j] == n) {
+                        arr2[0] = arr[i];
+                        arr2[1] = arr[j];
+
+                        foreach (int a in arr2) Console.Write($"{a} ");
+                    } 
+                }
+            }
+
+        }
+    }
+}
