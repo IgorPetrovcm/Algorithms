4476b86941f57bb43a7f6ba3d13f668883c1fd6a
igoradm <igorpetrov42ru@gmail.com>
2023-11-05 14:24:18 +0300

    Смог реверснуть число напрямую из односвязного списка: LeetCode/AddTwoNumbers2/Program.cs(7,18)

---
+++ LeetCode/AddTwoNumbers2/Program.cs
@@ -0,0 +1,50 @@
+﻿ namespace Program
+{
+    using System;
+    
+    class Program
+    {
+        public static int ReverseNum(ListNode node) 
+        {
+            int multiplier = 1;
+            int result = 0;
+            while (node != null) 
+            {
+                result += node.val * multiplier;
+                multiplier *= 10;
+                node = node.next;
+            }
+            return result;
+        }
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2) 
+        {
+            return l1;
+        }
+        static void Main(string[] args)
+        {
+            //------
+            ListNode l12 = new ListNode(3);
+            ListNode l11 = new ListNode(4,l12);
+            ListNode l1 = new ListNode(2, l11);
+            //------
+            ListNode l22 = new ListNode(4);
+            ListNode l21 = new ListNode(6,l22);
+            ListNode l2 = new ListNode(5,l21);
+
+            int revRes = ReverseNum(l1);
+            Console.Write(revRes);
+        } 
+    }
+
+
+    public class ListNode 
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) 
+        {
+            this.val = val;
+            this.next = next;
+        }
+  }
+}
