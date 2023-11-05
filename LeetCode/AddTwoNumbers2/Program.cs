734c0d6ab244430ee63a74f96e8513b52512af7f
igoradm <igorpetrov42ru@gmail.com>
2023-11-05 15:46:48 +0300

    Нашел новую ошибку, исправил костылям введя условия размерности числа суммы. Появилась новая ошибка с литкода, встречается снова

--- LeetCode/AddTwoNumbers2/Program.cs
+++ LeetCode/AddTwoNumbers2/Program.cs
@@ -16,33 +16,34 @@
             }
             return result;
         }
-        public static ListNode Nodes(int sum) 
+        public static ListNode Nodes(int sum, int length) 
         {
-            if (sum == 0) return null;
+            if (length == 0) return null;
             ListNode node = new ListNode();
             int unit = sum % 10;
             sum /= 10;
             node.val = unit;
-            node.next = Nodes(sum);
+            length -= 1;
+            node.next = Nodes(sum,length);
             return node;
         }
         public static ListNode AddTwoNumbers(ListNode l1, ListNode l2) 
         {
             int sum = ReverseNum(l1) + ReverseNum(l2);
-            ListNode sumNode = Nodes(sum);
+            ListNode sumNode = Nodes(sum,sum.ToString().Length);
             return sumNode;
 
         }
         static void Main(string[] args)
         {
             //------
-            ListNode l12 = new ListNode(3);
-            ListNode l11 = new ListNode(4,l12);
-            ListNode l1 = new ListNode(2, l11);
+            //ListNode l12 = new ListNode(3);
+            //ListNode l11 = new ListNode(4,l12);
+            ListNode l1 = new ListNode(0);
             //------
-            ListNode l22 = new ListNode(4);
-            ListNode l21 = new ListNode(6,l22);
-            ListNode l2 = new ListNode(5,l21);
+            //ListNode l22 = new ListNode(4);
+            //ListNode l21 = new ListNode(6,l22);
+            ListNode l2 = new ListNode(0);
 
             ListNode list = AddTwoNumbers(l1,l2);
 
