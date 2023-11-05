234b9a589817a81b515140caf79ec751cd30ab18
igoradm <igorpetrov42ru@gmail.com>
2023-11-05 16:56:58 +0300

    Нашел необычное решение, его маленьких разбор лежит в папке с решением в файле под названием Info

--- LeetCode/ReversInteger/Program.cs
+++ LeetCode/ReversInteger/Program.cs
@@ -12,9 +12,17 @@
                 int unit = x % 10;
                 x /= 10;                
                 xRev = xRev * 10 + unit;
+                if (xRev % 10 != unit) {Console.Write(0);return;}
             }
-            if (!(xRev is int)) { Console.Write(0); return; }       
             Console.Write(xRev);
+
+            /*int a = 2147483647;
+            Console.WriteLine(a.GetType().ToString());
+            Console.WriteLine((a+100).GetType().ToString());
+            Console.Write(a+100);
+            int b = 0;
+            if (Int32.TryParse((a + 100).ToString(),out b)) Console.WriteLine("OK");
+            Console.WriteLine(b);*/
         }
     }
 }
