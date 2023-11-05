b01ffc6d33ec72866525ba8d5202faad35ecdbb8
igoradm <igorpetrov42ru@gmail.com>
2023-11-05 18:16:51 +0300

    Решение задачи с литкод очень плохой вариант решения..

---
+++ LeetCode/FindTheWinnerOfanArrayGame/Program.cs
@@ -0,0 +1,50 @@
+﻿ namespace Program
+{
+    using System;
+    using System.Buffers;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1,2,3,4,5,6};
+            int k = 2;
+            int winCount = 0;
+            while (true) 
+            {              
+                if (winCount == k) break;  
+                if (arr[0] > arr[1]) 
+                {
+                    winCount++;
+                    int unit = arr[1];
+                    for (int i = 1; i < arr.Length;i++) 
+                    {
+                        if (i == arr.Length-1) 
+                        {
+                            arr[i] = unit;
+                            break;
+                        }
+                        arr[i] = arr[i+1];
+                        
+                    }
+                }
+                if (arr[0] < arr[1])
+                {
+                    winCount = 0;
+                    int unit = arr[0];
+                    arr[0] = arr[1];
+                    for (int i = 1; i < arr.Length;i++) 
+                    {
+                        if (i == arr.Length-1) 
+                        {
+                            arr[i] = unit;
+                            break;
+                        }
+                        arr[i] = arr[i+1];
+                    }
+                }
+            }
+            Console.Write(arr[0]);
+        }
+    }
+}
