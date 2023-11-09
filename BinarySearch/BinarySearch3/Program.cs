65e58f5ced9d5e700b5886c0603e87de4ae13e97
igoradm <igorpetrov42ru@gmail.com>
2023-11-09 22:36:11 +0300

    Алгоритм поиска приблизительных значений бинарным поиском

---
+++ BinarySearch/BinarySearch3/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace BinarySearch3 
+{
+    using System;
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Random rand = new Random();
+            byte sizeZero = (byte)rand.Next(0,10);
+            byte sizeOne = (byte)rand.Next(0,10);
+
+            byte[] arr = new byte[(byte)(sizeZero + sizeOne)];
+
+            for (int i = 0; i < sizeZero;i++){
+                arr[i] = 0;
+                Console.Write(arr[i] + " ");
+            }
+            for (int i = sizeZero; i < arr.Length;i++){
+                arr[i] = 1;
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+
+            short length = -1;
+            byte right = (byte)arr.Length;
+
+            while ((right - length) != 1) 
+            {
+                byte index = (byte)((right + length)/2);
+
+                if (arr[index] == 0) {
+                    length = index;
+                }
+                else {
+                    right = index;
+                }
+            } 
+            Console.WriteLine(length + " " + right);
+            Console.WriteLine(arr[length] + " " + arr[right]);
+        }
+    }
+}
---
+++ BinarySearch/BinarySearch5/Program.cs
@@ -0,0 +1,23 @@
+﻿ namespace Program
+{
+    using System;
+    using System.Globalization;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1,3,3,4,6,7,7,12};
+            int searchValue = 5;
+            int head = 0;
+            int tail = arr.Length-1;
+            while (head + 1 != tail) 
+            {
+                int value = (head + tail) /2;
+                if (arr[value] > searchValue) tail = value;
+                else head = value; 
+            }
+            Console.Write(arr[head] + " " + arr[tail]);
+        }
+    }
+}
