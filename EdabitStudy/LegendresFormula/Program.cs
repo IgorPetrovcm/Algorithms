b207aa7cf595dbc07b29dccafbf49e9719e425cc
igoradm <igorpetrov42ru@gmail.com>
2023-11-12 22:19:09 +0300

    РЕшена задача с Edabit

---
+++ EdabitStudy/LegendresFormula/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Program 
+{
+    class Program 
+    {
+        static void Main() 
+        {
+            int n = 128;
+            int p = 2;
+            
+        }
+    }
+}
