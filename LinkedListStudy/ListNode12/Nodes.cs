8c14a12766e651e42c87185488ccfe6455d1e2b6
igoradm <igorpetrov42ru@gmail.com>
2023-11-12 22:18:34 +0300

    Решенные две задачи на односвязные списки

---
+++ LinkedListStudy/ListNode12/Nodes.cs
@@ -0,0 +1,34 @@
+namespace ListNode 
+{
+	using System;
+	class Node 
+	{
+		public int val;
+		public Node next;
+		public Node(int val=0, Node next=null) 
+        {
+            this.val = val;
+            this.next = next;
+        }
+	}
+	class NodeList 
+	{
+		public Node head;
+		public Node tail;
+		public NodeList() {}
+		public void Add(int val) 
+		{
+			Node node = new Node(val);
+			if (head == null) 
+			{
+				head = node;
+				head.next = node;
+				tail = node;
+				tail.next = node;
+				return;
+			}
+			tail.next = node;
+			tail = node;
+		}
+	}
+}
---
+++ LinkedListStudy/ListNode12/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ListNode 
+{
+	using System;
+	class Program 
+	{
+        public static NodeList Sorting(NodeList l1) 
+        {
+            int countError = 0;            
+            while (true) 
+            {   
+                countError = 0;
+                Node node = l1.head;
+                while (node.next != null) 
+                {
+                    if (node.val > node.next.val) {
+                        int val = node.next.val;
+                        node.next.val = node.val;
+                        node.val = val;
+                        node = node.next;
+                        countError++;
+                        continue;
+                    }
+                    node = node.next;
+                }
+                if (countError == 0) return l1;                
+            }
+        }
+		static void Main(string[] args) 
+		{
+            Random rand = new Random();
+            NodeList l1 = new NodeList();
+			for (int i = 0; i < 4; i++) 
+            {
+                l1.Add(rand.Next(-5,8));
+            }
+
+            NodeList list = Sorting(l1);
+            Node node = list.head;
+            while (node != null) 
+            {
+                Console.Write(node.val);
+                node = node.next;
+            }
+		}
+	}
+}
