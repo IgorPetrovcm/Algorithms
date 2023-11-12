25b6fa13f02d6f70d97b780cc420eee08b8200b0
igoradm <igorpetrov42ru@gmail.com>
2023-11-12 22:37:49 +0300

    Создал библиотеку односвязного спика

---
+++ LinkedListStudy/LibraryNodeList/NodeList/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace NodeList;
+
+public class Node
+{
+    public int val;
+    public Node next;
+    public Node(int val=0,Node next=null) 
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class NodeList 
+{
+    public Node head;
+    public Node tail;
+    public NodeList() {}
+
+    public void Add(int val) 
+    {
+        Node node = new Node(val);
+        if (head == null) 
+        {
+            head = node;
+            head.next = node;
+            tail = node;
+            tail.next = node;
+            return;
+        }
+        tail.next = node;
+        tail = node;
+    }
+}
