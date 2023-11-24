fc8709b513cc6d870d566033b9b771c547fd9ad7
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-11-24 14:39:36 +0300

    Add files via upload

---
+++ Stack/Stack3/Program.cs
@@ -0,0 +1,29 @@
+﻿ namespace Program
+{
+    using System;
+    using System.Text.RegularExpressions;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFile = @"text.txt";
+            Stack<string> stack = new Stack<string>();
+            using (var reader = new StreamReader(textFile)) 
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null) 
+                {
+                    string regex = @"\w{1,40}";
+                    foreach (Match match in Regex.Matches(line,regex)) 
+                    {
+                        stack.Push(match.Value);
+                    }
+                }            
+            }
+            foreach (var r in stack) 
+            {
+                System.Console.WriteLine(r);
+            }
+        }
+    }
+}
