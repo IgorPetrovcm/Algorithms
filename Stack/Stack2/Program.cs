b3fd1858fc7207a8e26742398d95f94d52fabb03
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-11-24 14:26:07 +0300

    Add files via upload

---
+++ Stack/Stack2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Program 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            int sizeArr = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[sizeArr];
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < sizeArr; i++ ) 
+            {
+                stack.Push(i+1);
+            }
+            for (int i = 0; i < sizeArr; i++ ) 
+            {
+                arr[i] = stack.Peek();
+                System.Console.WriteLine(arr[i]);
+                stack.Pop();
+            }
+        }
+    }
+}
