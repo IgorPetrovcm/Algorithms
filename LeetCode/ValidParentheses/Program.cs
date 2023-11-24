f88df1ebb651a78fea616b380d294ea38b3d9d33
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-11-24 19:03:19 +0300

    Add files via upload

---
+++ LeetCode/ValidParentheses/Program.cs
@@ -0,0 +1,43 @@
+﻿ namespace Program
+{
+    using System;
+    using System.Collections;
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "(((()))))";
+            Stack<char> stack = new Stack<char>();
+            char[] symbols = s.ToCharArray();
+            for (int i = 0; i < symbols.Length;i++) 
+            {
+                if (symbols[i] != '(' || symbols[i] != '[' || symbols[i] != '{') 
+                {
+                    if (stack.Count == 0) {
+                        System.Console.WriteLine("false");
+                        return;
+                    } 
+                    if (symbols[i] == ')') if (stack.Peek() == '(') {
+                        stack.Pop();
+                        continue;
+                    }
+                    if (symbols[i] == ']') if (stack.Peek() == '[') {
+                        stack.Pop();
+                        continue;
+                    }
+                    if (symbols[i] == '}') if (stack.Peek() == '}') {
+                        stack.Pop();
+                        continue;
+                    }
+                }
+                stack.Push(symbols[i]);
+            }
+            if (stack.Count != 0) System.Console.WriteLine("false");
+            else System.Console.WriteLine("true");
+
+        }
+    }
+}
