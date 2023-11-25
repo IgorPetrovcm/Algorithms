2e4e0710f6a3a38dec70656aa76cca8d687dabd6
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-11-25 17:19:07 +0300

    Add files via upload

---
+++ Stack/manual_implementation_manual_test/Program.cs
@@ -0,0 +1,28 @@
+﻿ namespace Program
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*MinStack stack = new MinStack();
+            for (int i = 0; i < 5; i++) 
+            {
+                stack.Push(i);
+            }
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length;i++) 
+            {
+                arr[i] = stack.Top();
+                stack.Pop();            
+            }*/
+
+            MinStack stack = new MinStack();
+            stack.Push(-2);
+            stack.Push(0);
+            stack.Push(3);
+            stack.Push(-4);
+            System.Console.WriteLine(stack.getMin());
+        }
+    }
+}
