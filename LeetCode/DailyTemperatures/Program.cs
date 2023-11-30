3670d1d2dc557ef989b6f4f9f6f4b6c6289570d5
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-11-30 18:03:37 +0300

    Add files via upload

---
+++ LeetCode/DailyTemperatures/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Program;
+class Program 
+{
+    static void Main() 
+    {
+        int[] temperatures = {73, 74,75,71,69,72,76,73};
+        int[] output = new int[temperatures.Length];
+        for (int i = 0; i < temperatures.Length;i++) 
+        {
+            for (int j = i+1 ; j < temperatures.Length; j++) 
+            {
+                if (temperatures[j] > temperatures[i])  
+                {
+                    output[i] = j - i;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < output.Length;i++) System.Console.WriteLine(output[i]);
+    }
+}
