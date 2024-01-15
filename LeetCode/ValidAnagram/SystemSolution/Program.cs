1b52f2c596297c4f0d3e5cf79a8e25f179b6bbba
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-15 15:44:53 +0300

    Решение обгоняет в пике 99.98 процентов пользователь, но с памятью плохо

---
+++ LeetCode/ValidAnagram/SystemSolution/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SystemSolution;
+
+
+public class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine(Validate("aab","bba"));
+    }
+
+    static bool Validate(string s, string t)
+    {
+        Array.Sort(s.ToCharArray());
+        Array.Sort(t.ToCharArray());
+
+        return Enumerable.SequenceEqual(s,t);
+    }
+}
