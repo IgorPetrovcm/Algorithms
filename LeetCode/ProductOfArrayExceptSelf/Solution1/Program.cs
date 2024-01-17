fce454b1772d1865932d79c1d1497f4155725963
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-17 21:58:43 +0300

    Solution1 ready and working

---
+++ LeetCode/ProductOfArrayExceptSelf/Solution1/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Solution1;
+
+
+public class Program 
+{
+    static void Main() 
+    {
+        int[] arr = TaskFunction(new int[] {1,2,3,4});
+    }
+
+    static int[] TaskFunction(int[] nums)
+    {
+    /*int n = nums.Length;
+    int[] answer = new int[n];
+
+    // Создаем промежуточные массивы для хранения произведений чисел слева и справа от текущего элемента
+    int[] leftProduct = new int[n];
+    int[] rightProduct = new int[n];
+
+    // Вычисляем произведение чисел слева от текущего элемента
+    leftProduct[0] = 1;
+    for (int i = 1; i < n; i++) {
+        leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
+    }
+
+    // Вычисляем произведение чисел справа от текущего элемента
+    rightProduct[n - 1] = 1;
+    for (int i = n - 2; i >= 0; i--) {
+        rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
+    }
+
+    // Вычисляем ответ как произведение чисел слева и справа от текущего элемента
+    for (int i = 0; i < n; i++) {
+        answer[i] = leftProduct[i] * rightProduct[i];
+    }
+
+    return answer;*/
+
+        int[] left = new int[nums.Length];
+        int[] right = new int[nums.Length];
+
+        left[0] = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            left[i] = left[i-1] * nums[i-1];
+        }
+
+        right[nums.Length-1] = 1;
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            right[i] = right[i+1] * nums[i+1];
+        }
+
+        int[] res = new int[nums.Length];
+        for (int i = 0; i < res.Length; i++)
+        {
+            res[i] = left[i] * right[i];
+        }
+
+        return res;
+    }
+}
--- LeetCode/ProductOfArrayExceptSelf/Solution1/obj/Debug/net8.0/Solution1.AssemblyInfo.cs
+++ LeetCode/ProductOfArrayExceptSelf/Solution1/obj/Debug/net8.0/Solution1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ProductOfArrayExceptSelf")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Solution1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c15c47affa02be604bb08fb9c71a4ca4568acdbe")]
-[assembly: System.Reflection.AssemblyProductAttribute("ProductOfArrayExceptSelf")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ProductOfArrayExceptSelf")]
+[assembly: System.Reflection.AssemblyProductAttribute("Solution1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Solution1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
