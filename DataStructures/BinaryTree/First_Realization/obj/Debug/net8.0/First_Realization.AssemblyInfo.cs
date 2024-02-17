6967021ab6dbcf1318e1c77f14077582ce7dcd8f
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-02-17 16:51:53 +0300

    Решение Решета Эратосфена

--- DataStructures/BinaryTree/First_Realization/obj/Debug/net8.0/First_Realization.AssemblyInfo.cs
+++ DataStructures/BinaryTree/First_Realization/obj/Debug/net8.0/First_Realization.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("First_Realization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ed9361adbfca2ed3370ffa7cc16dd99bddf96e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1576542000816f96ec5ad42f49dcea7007920a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("First_Realization")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First_Realization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ WorkWith_/РешетаЭратосфена/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Program;
+
+
+public class Program
+{
+    static void Main()
+    {
+        Algorithm(120);
+    }
+
+    public static void Algorithm(int n)
+    {
+        bool[] s = new bool[n];
+
+        GetPrimeNumbers(s, 1);
+    }
+
+    public static void GetPrimeNumbers(bool[] s, int index)
+    {
+        int firstIndex = index;
+
+        for (int i = index + 1; i < s.Length && index == firstIndex; i++)
+        {
+            if (s[i] == false)
+            {
+                index = i;
+                Console.WriteLine(index);
+
+                break;
+            }
+        }
+
+        if (firstIndex == index)
+            return;
+        
+        for (int i = index + 1; i < s.Length; i++)
+        {
+            if (s[i] == false)
+            {
+                if (i % index == 0) 
+                {
+                    s[i] = true;
+                }
+            }
+        }
+
+        GetPrimeNumbers(s, index);
+    }
+}
